8a660ea8a3b1271c13af49f192734ca9ae00e2e5
Anton Georgiev <anton4o123@gmail.com>
2015-03-15 12:28:49 +0200

    Added click handlers for the buttons. To make POST requests

--- scripts/sampleAPITester.js
+++ scripts/sampleAPITester.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
 	"use strict"
-
 	var url = "http://private-38c95-equationsolver.apiary-mock.com/";
-	$("#testClicker").click(function() {
-		$.get(url, {
-			"anton": "haha"
+
+	$("#submitEquation").click(function() {
+		$.post(url + "equations", {
+			"user": $("userEquation").val(),
+			"equation": $("#equation").val()
+		}).then(function(response) {
+			console.log(response);
+		});
+	});
+
+	$("#submitFunction").click(function() {
+		$.post(url + "functions", {
+			"user": $("userFunction").val(),
+			"function": $("#function").val()
 		}).then(function(response) {
 			console.log(response);
 		});
