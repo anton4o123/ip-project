b8701ec7bb1aeb42555008613ddb32a7539e5bc1
Anton Georgiev <anton4o123@gmail.com>
2015-03-15 12:39:53 +0200

    Added function for appending objects from responses to table bodies.

--- scripts/sampleAPITester.js
+++ scripts/sampleAPITester.js
@@ -1,6 +1,33 @@
+function append_item(table_body, item, equation) {
+	var row = $("<tr/>");
+	row.append($("<td/>").text(item.id));
+	row.append($("<td/>").text(item.user));
+	row.append($("<td/>").text(item.created_at));
+	if(equation) {
+		row.append($("<td/>").text(item.equation));
+		row.append($("<td/>").text(item.solution));
+	} else {
+		row.append($("<td/>").text(item.function));
+		row.append($("<td/>").text(item.graphics));
+	}
+	table_body.append(row);
+}
+
 $(document).ready(function() {
 	"use strict"
 	var url = "http://private-38c95-equationsolver.apiary-mock.com/";
+
+	$.get(url + "equations").then(function(response) {
+		$.each(response, function() {
+			append_item($("#equation-table tbody"), this, true);
+		});
+	});
+
+	$.get(url + "functions").then(function(response) {
+		$.each(response, function() {
+			append_item($("#function-table tbody"), this, false);
+		});
+	});
 
 	$("#submitEquation").click(function() {
 		$.post(url + "equations", {
