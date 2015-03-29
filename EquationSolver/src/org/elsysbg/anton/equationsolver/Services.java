82520f6401b1f0955dfa992fe8cd53d6ab7d3030
Anton Georgiev <anton4o123@gmail.com>
2015-03-29 12:41:59 +0300

    Added services for rest purpose

---
+++ EquationSolver/src/org/elsysbg/anton/equationsolver/Services.java
@@ -0,0 +1,38 @@
+package org.elsysbg.anton.equationsolver;
+
+import org.elsysbg.anton.equationsolver.service.ArticleService;
+import org.elsysbg.anton.equationsolver.service.EquationService;
+import org.elsysbg.anton.equationsolver.service.FunctionService;
+
+public class Services {
+	private static EquationService equationService;
+	private static FunctionService functionService;
+	private static ArticleService articleService;
+	public synchronized static EquationService getEquationService() {
+		if (equationService == null) {
+			equationService = new EquationService();
+		}
+		return equationService;
+	}
+	public static void setEquationService(EquationService equationService) {
+		Services.equationService = equationService;
+	}
+	public static FunctionService getFunctionService() {
+		if (functionService == null) {
+			functionService = new FunctionService();
+		}
+		return functionService;
+	}
+	public static void setFunctionService(FunctionService functionService) {
+		Services.functionService = functionService;
+	}
+	public static ArticleService getArticleService() {
+		if (articleService == null) {
+			articleService = new ArticleService();
+		}
+		return articleService;
+	}
+	public static void setArticleService(ArticleService articleService) {
+		Services.articleService = articleService;
+	}
+}
